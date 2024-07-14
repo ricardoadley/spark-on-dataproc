4dd1a7044095851633c9a936d253af4aea4598e4
Ricardo Adley <ricardo.sena@ccc.ufcg.edu.br>
2024-07-14 11:41:25 -0300

    envio scripts

--- generos.py
+++ generos.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import count
 
-spark = SparkSession.builder.appName("NYC Bike Trips").getOrCreate()
+spark = SparkSession.builder.appName("Dell AI").getOrCreate()
 df = spark.read.parquet("gs://dell-dataproc-4/GCStoGCS/*.snappy.parquet", header=True, inferSchema=True)
 
 genders = df.groupBy("gender").agg(count("gender").alias("count"))
